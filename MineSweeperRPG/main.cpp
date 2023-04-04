04fec7c483cdbcaca8f587eb92cd0324d4146aa4
수민 허 <p10453@naver.com>
2023-04-04 20:40:20 +0900

    메뉴 선택 화살표 이동 기능 구현

--- MineSweeperRPG/main.cpp
+++ MineSweeperRPG/main.cpp
@@ -1,28 +1,37 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <conio.h>
+
+// 키보드 값
+#define UP     0
+#define DOWN   1
+#define LEFT   2
+#define RIGHT  3
+#define SUBMIT 4	// 선택 (스페이스바
 
 
 /* 함수 선언 */
-void init();
+void init();		// 콘솔 화면 크기 지정
+int keyControl();
 void descOneDraw();		// 게임시작 정보 출력
 void titleDraw();		// 타이틀 출력
-void menuDraw();		// 메인 메뉴 출력
-void gotoxy(int, int);	//마우스 커서를 이동
+int menuDraw();		// 메인 메뉴 출력
+void gotoxy(int, int);	// 마우스 커서를 이동
 
 int main()
 {
 	titleDraw();
-	menuDraw();
 	descOneDraw();
+	int menuCode = menuDraw();
 	system("pause");
 }
 
 /* 함수 구현 */
+
 void init()
 {
-	system("mode con cols=120 lines=40 | title minesweeper_rpg");		// 콘솔창 크기 초기화
+	system("mode con cols=120 lines=30 | title minesweeper_rpg");		// 콘솔창 크기 초기화
 }
-
 
 void gotoxy(int x, int y)
 {
@@ -33,7 +42,7 @@ void gotoxy(int x, int y)
 	SetConsoleCursorPosition(consoleHandle, pos);
 }
 
-void titleDraw()	// 타이틀 제목 출력
+void titleDraw()
 {
 	char print_temp[256];
 	FILE* rfp = NULL;
@@ -53,14 +62,66 @@ void titleDraw()	// 타이틀 제목 출력
 
 }
 
-void menuDraw()
+int menuDraw()
 {
-	gotoxy(35 - 2, 30);
+	int x = 35;
+	int y = 30;
+	gotoxy(x - 2, y);
 	printf("> 게임시작");
-	gotoxy(35, 31);
+	gotoxy(x, y+1);
 	printf("게임정보");
-	gotoxy(35, 32);
+	gotoxy(x, y+2);
 	printf("  종료  ");
+
+	while (true)	// 키보드 입력을 무한정 받고 기다리고 하는 부분
+	{
+		int n = keyControl();		// 키보드 이벤트를 키값으로 받아오기 // 키보드의 키코드를 임시로 저장하는 변수
+		switch (n) {
+			case UP: {
+				if (y > 30) {	// 메뉴 위로 벗어나지 않기
+					gotoxy(x - 2, y);	// ">"를 두칸 이전에 출력하기 위해 x-2 를 함
+					printf(" ");		// 원래 있던 ">"를 지움
+					gotoxy(x - 2, --y);	// 새 위치로 이동하여
+					printf(">");		// 다시 ">" 그리기
+				}
+				break;
+			}
+			case DOWN: {
+				if (y < 32) {	// 메뉴 아래로 벗어나지 않기
+					gotoxy(x - 2, y);
+					printf(" ");
+					gotoxy(x - 2, ++y);
+					printf(">");
+				}
+				break;
+			}
+			case SUBMIT: {
+				return y - 30;
+				// y 시작 위치가 30였으므로 y-30를 하면 0, 1, 2 세 숫자를 받아올 수 있다.
+			}
+		}
+	}
+}
+
+int keyControl()
+{
+	char temp = _getch();	// 엔터를 입력하지 않아도 키가 눌리면 바로 변환해주는 입력 함수
+
+	if		(temp == 'w' || temp == 'W') {
+		return UP;
+	}
+	else if (temp == 'a' || temp == 'A') {
+		return LEFT;
+	}
+	else if (temp == 's' || temp == 'S') {
+		return DOWN;
+	}
+	else if (temp == 'd' || temp == 'D') {
+		return RIGHT;
+	}
+	else if (temp == ' ') {		// 스페이스바(공백)이 선택 버튼
+		return SUBMIT;
+	}
 }
 
 void descOneDraw()
